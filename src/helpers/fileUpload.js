e1dfd919d12436178e5ad7b923f0b144b09b793b
Gerardo Mayboca <59183708+Guerrimau@users.noreply.github.com>
2020-07-11 08:51:08 -0700

    Se removio el link de cloudinary

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -2,7 +2,7 @@
     
 export const fileUpload = async ( file ) => {
     
-    const cloudUrl = 'https://api.cloudinary.com/v1_1/dblwegpaz/upload';
+    const cloudUrl = '';
     
     const formData = new FormData();
     formData.append('upload_preset','react-journal');
